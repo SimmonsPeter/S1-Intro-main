752a9ca949b2cebaef5ecdb437a0905c2fa1f0bb
SimmonsPeter <0353384@cegepoutaouais.qc.ca>
2023-01-11 15:55:09 -0500

    Début javascript

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function ajouter() {
+    var item = document.getElementById("desc").value;
+    document.getElementById("maListe").append(item);
+    alert("L'item est bien ajouté")
+}
+
 console.log("Bonjour");
